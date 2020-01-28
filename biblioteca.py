ea760fbfe5d728dc2ebc1ccb19548a348d7711a0
ApoloXXV <57403876+ApoloXXV@users.noreply.github.com>
2020-01-28 17:56:11 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,15 +1,18 @@
 class libro:
     titulo=''
     autor=''
-    
+
     def escribir(self):
-      
+          
         archivo= open("archivo.txt","a+")
         archivo.write("Titulo: "+libro1.titulo + " " +"Autor: "+libro1.autor + "\n")
-    
-
-      
-libro1=libro()
+        
+    def leer(self):
+        archivo=open("archivo.txt","r")
+        print(archivo.read())
+   
+libro1= libro() 
 libro1.titulo=(input('escriba un libro '))     
 libro1.autor=input('escriba el autor ')
 libro1.escribir()
+libro1.leer()
