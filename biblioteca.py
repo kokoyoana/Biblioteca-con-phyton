156d4a43aa3ad6485dfc00433b1ec57586f4338b
kokoyoana <koko-yoana@hotmail.es>
2020-01-28 17:55:10 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -7,7 +7,8 @@ class libro:
         archivo= open("archivo.txt","a+")
         archivo.write("Titulo: "+libro1.titulo + " " +"Autor: "+libro1.autor + "\n")
     
-     
+
+      
 libro1=libro()
 libro1.titulo=(input('escriba un libro '))     
 libro1.autor=input('escriba el autor ')
