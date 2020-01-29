5aefbfe956fee47ff576dfa4c06b37f1ef87e4c0
kokoyoana <koko-yoana@hotmail.es>
2020-01-29 18:02:18 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -10,10 +10,23 @@ class libro:
     def leer(self):
         archivo=open("archivo.txt","r")
         print(archivo.read())
+        
+        
    
+    def sobre(self):
+        a=input('escriba un titulo ')
+        archivo= open('archivo.txt')
+  
+        if a==archivo.readlines(archivo) :
+           print('esta repetido')
+        else:
+           print('libro guardado')
+         
+
 libro1= libro() 
 libro1.titulo=(input('escriba un libro '))     
 libro1.autor=input('escriba el autor ')
 libro1.escribir()
 libro1.leer()
+libro1.sobre()
 
