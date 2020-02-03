c66c46af10c0c93457585d2743f1ac15b7f9955f
kokoyoana <koko-yoana@hotmail.es>
2020-02-03 17:16:38 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -6,9 +6,11 @@ class libro:
           
         archivo= open("archivo.txt","a+")
         archivo.write("Titulo: "+libro1.titulo + " " +"Autor: "+libro1.autor + "\n")
+     
         
     def leer(self):
         archivo=open("archivo.txt","r")
+     
         print(archivo.read())
         
         
