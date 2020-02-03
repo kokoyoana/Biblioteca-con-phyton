ea2e37bafc48f54c2c19524b2796c9703a606347
ApoloXXV <catadorverde@gmail.com>
2020-02-03 17:25:29 +0100

    Create Biblioteca.py

---
+++ VILLATOMILLO/Biblioteca.py
@@ -0,0 +1,32 @@
+class Libro:
+    titulo=''
+    autor=''
+    
+    def escribir(self):
+        self.titulo=(input('escriba un libro '))
+        if self.existe():
+                print("El Libro Ya esta en la Biblioteca")
+        else:
+                self.autor=(input('escriba el autor '))
+                archivo= open("archivo.txt","a+")
+                archivo.write("Titulo: "+self.titulo + " " +"Autor: "+ self.autor + "\n")
+            
+    def leer(self):
+        print("hola")
+        archivo=open("archivo.txt","r")
+        print(archivo.read())
+
+
+    def existe(self):
+        archivo=str(open("archivo.txt","r").readlines())
+        self.titulo
+        if self.titulo in archivo:
+            return True
+        else:
+            return False
+
+
+        
+
+#libro1= Libro() 
+#libro1.menu()
