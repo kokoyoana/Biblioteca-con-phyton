e88de2c179e7c997a4f146de3bcbf93b9d4523cf
ApoloXXV <57403876+ApoloXXV@users.noreply.github.com>
2020-02-05 15:58:12 +0100

    Update Biblioteca.py

--- VILLATOMILLO/Biblioteca.py
+++ VILLATOMILLO/Biblioteca.py
@@ -3,52 +3,42 @@ class Libro:
     autor=''
     
     def escribir(self):
-        self.titulo = (input('Escriba un libro ').casefold())  
-        if self.newexiste():
-                print("El libro ya está en la Biblioteca")
+        self.titulo=(input('Escriba un libro ').casefold())  
+        if self.existe():
+                print("El Libro ya está en la Biblioteca")
         else:
-                self.autor = (input('Escriba el autor ').casefold())  
-                archivo = open("archivo.txt","a+")
-                archivo.write("Titulo: " + self.titulo + " " + "Autor: " + self.autor + "\n")
+                self.autor=(input('escriba el autor ').casefold())  
+                archivo= open("archivo.txt","a+")
+                archivo.write("Titulo: "+self.titulo + " " +"Autor: "+ self.autor + "\n")
             
     def leer(self):
-        archivo = open("archivo.txt","r")
+        print("hola")
+        archivo=open("archivo.txt","r")
         print(archivo.read())
 
 
     def existe(self):
-        archivo = str(open("archivo.txt","r").readlines())
+        archivo=str(open("archivo.txt","r").readlines())
         self.titulo
         if self.titulo in archivo:
             return True
         else:
             return False
+        
+ 
+    def eliminar(self):
+        archivo = open("archivo.txt", "r")
+        lineas = archivo.readlines()#lineas en un array / 
+        i =0
+        while i<len(lineas):
+            print(i , lineas[i])
+            i= i+1
+        indice = int(input("Indicar indice que desea borrar"))
+
+        del lineas [indice]
+        archivo = open("archivo.txt", "w")
+        for valor in lineas:
+            archivo.write(valor)
     
-    def buscar(self):
-        archivo = str(open("archivo.txt","r").readlines()) 
-        b = input('¿Qué título buscas? ')
-        x = archivo.find(b)
-        if x > 0:
-            print('El libro "' + b + '" ESTÁ en la Biblioteca.')
-        else:
-            print('El libro "' + b + '" NO EXISTE en la Biblioteca.')
-            
-    def newexist(self):
-        archivo = open ("archivo.txt", "r") 
-        palabra = input("Que Libro deseas? ")
-        linea = " "
-        count = 1
-        existe= False
-        while (linea):
-            linea = archivo.readline()
-            L = linea.split()
-            if palabra in L:
-                print("Se Encuentra en ", count, ":", linea)
-                count += 1
-                existe=True
-        if existe==False:
-            print("No existe")
-        archivo.close()
-                
 
         
