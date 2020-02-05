e494dffe81332be92c683cf6a9bd8cb3f69e897f
AlionaF5 <57355237+AlionaF5@users.noreply.github.com>
2020-02-05 16:09:52 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,52 +1,43 @@
-class libro:
+class Libro:
     titulo=''
     autor=''
-
-    def escribir(self):
-          
-        archivo= open("archivo.txt","a+")
-        archivo.write("Titulo: "+libro1.titulo + " " +"Autor: "+libro1.autor + "\n")
-     
-        
-    def leer(self):
-        archivo=open("archivo.txt","r")
-     
-        print(archivo.read())
-        
-        
-   
     
-        
+    def escribir(self):
+        self.titulo=(input('Escriba un libro ').casefold())  
+        if self.existe():
+                print("El Libro ya está en la Biblioteca")
+        else:
+                self.autor=(input('escriba el autor ').casefold())  
+                archivo= open("archivo.txt","a+")
+                archivo.write("Titulo: "+self.titulo + " " +"Autor: "+ self.autor + "\n")
             
-    def buscar(self):
-       archivo=str(open("archivo.txt","r").readlines()) 
-       b=input('que titulo buscas')
-       archivo.index(b)
-       print('lo encontro')
-       
-    def newexist(self):
-        archivo = open ("archivo.txt", "r") 
-        palabra = input("Que Libro deseas? ")
-        linea = " "
-        count = 1
-        existe= False
-        while (linea):
-            linea = archivo.readline()
-            L = linea.split()
-            if palabra in L:
-                print("Se Encuentra en ", count, ":", linea)
-                count += 1
-                existe=True
-        if existe==False:
-            print("No existe")
-        archivo.close()
-         
+    def leer(self):
+        print("hola")
+        archivo=open("archivo.txt","r")
+        print(archivo.read())
 
-libro1= libro() 
-libro1.titulo=(input('escriba un libro ').casefold())     
-libro1.autor=input('escriba el autor '.casefold())
-libro1.escribir()
-libro1.leer()
-libro1.buscar()
-libro1.newexist()
+
+    def existe(self):
+        archivo=str(open("archivo.txt","r").readlines())
+        self.titulo
+        if self.titulo in archivo:
+            return True
+        else:
+            return False
+        
+ 
+    def eliminar(self):
+        archivo = open("archivo.txt", "r")
+        lineas = archivo.readlines()#lineas en un array / 
+        i =0
+        while i<len(lineas):
+            print(i , lineas[i])
+            i= i+1
+        indice = int(input("Indicar indice que desea borrar"))
+
+        del lineas [indice]
+        archivo = open("archivo.txt", "w")
+        for valor in lineas:
+            archivo.write(valor)
+    
 
